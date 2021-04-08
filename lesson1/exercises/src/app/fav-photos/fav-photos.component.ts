09c8b144333250ef79615667e17795d6effaf9dc
Steve Dennick <stdennick@gmail.com>
2021-04-08 17:20:43 -0500

    finished exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Awkward band photos';
+  image1 = 'https://static.boredpanda.com/blog/wp-content/uploads/2017/03/funny-awkward-metal-bands-photos-3-58cb9b219cb75__605.jpg';
+  image2 = 'https://static.boredpanda.com/blog/wp-content/uploads/2017/03/funny-awkward-metal-bands-photos-51-58cbb17161be6__605.jpg';
+  image3 = 'https://static.boredpanda.com/blog/wp-content/uploads/2019/02/awkward-metal-band-photos-25-5c54698870239__605.jpg';
 
   constructor() { }
 
